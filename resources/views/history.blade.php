5e17f5320dc0b63b332f5d02a22a8ab9e018f425
Deion Menor <deionmenor@gmail.com>
2019-02-24 03:44:54 +0800

    Hardcode History styles

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -2,28 +2,56 @@
 
 @section('content')
 
+<style>
+
+.day {
+    font-family: "HWT Slab Antique";
+    font-size: 75px;
+    display: flex;
+    justify-content: center;
+    margin-top: 28px;
+    margin-bottom: 8px;
+}
+
+.title {
+    font-family: "Rift Soft";font-size: 20px;display: flex;justify-content: center;margin-top: 32px;
+}
+
+.four-row-card {
+    background-color: #434244;color: #FEFEFE;height: 260px;min-width: 260px;margin-bottom: 50px;
+}
+
+.four-row-card.special {
+    border: 1px solid #434244;
+    color: #434244;
+    background-color: #FEFEFE;
+    height: 258px;
+    min-width: 260px;
+}
+
+.four-row-card .month {
+    font-family: "HWT Slab Antique";
+    font-size: 35px;display: flex;justify-content: center;margin-bottom: 33px;
+}
+
+</style>
 <div class="container">
     <div class="row justify-content-center">
 
+        <div class="four-column-row" style="display: flex;justify-content: space-between;align-items: stretch;flex-direction: row;flex-wrap: wrap;">;
+             <div class='four-row-card' style="background-color: #434244;color: #FEFEFE;height: 260px;min-width: 260px;margin-bottom: 50px;">
+                     <p class="title" style="font-size: 20px;display: flex;justify-content: center;margin-top: 32px;">
+                         "Untitled"</p>
+                    <p class="day" style="font-size: 75px;display: flex;justify-content: center;margin-top: 28px;margin-bottom: 8px;">
+                         23</h1>
+                     <h5 class="month" style="font-size: 35px;display: flex;justify-content: center;margin-bottom: 33px;">
+                         Feb</h5>
+              </div>
+        </div>
 
-        <!-- <div style="height: 400px;">
-               <div>
-                    <h1  style="font-size:130px;text-align:center">Get something done today!</h1>
-               </div>
-        </div> -->
 
-
-        <div class="card">
-             <div class="col-sm-3" >
-
-               HISTORY TIME
-            </div>
-
+         </div>
      </div>
-
-    </div>
-</div>
-
 
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,6 +102,7 @@
         <main class="py-4">
             @yield('content')
         </main>
+      
     </div>
 </body>
 </html>
