632cf8c25cd93b4ac95ce638c24cdaa48d4e0d33
aaroncolesee <aaron.see@obf.ateneo.edu>
2019-02-23 14:02:55 +0800

    created initial database tables

--- database/migrations/2019_02_23_055756_create_activities_table.php
+++ database/migrations/2019_02_23_055756_create_activities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateActivitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('activities', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string("goal")->nullable();
-            $table->text("image")->nullable();
-            $table->rememberToken();
+            $table->string('genre');
+            $table->unsignedInteger('entry_id');
+            $table->foreign('entry_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -33,6 +30,10 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('activities', function (Blueprint $table) {
+            $table->dropForeign(['entry_id']);
+        });
+
+        Schema::dropIfExists('activities');
     }
 }
