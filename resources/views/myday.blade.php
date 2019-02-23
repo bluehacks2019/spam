62bd5cc2b9e8db304ef0b541f39bcbd9ae29a390
Deion Menor <deionmenor@gmail.com>
2019-02-24 01:07:44 +0800

    Change My Day title to card header

--- resources/views/myday.blade.php
+++ resources/views/myday.blade.php
@@ -2,29 +2,45 @@
 
 @section('content')
 
+
 <div class="container">
 
-              <div class = "row">
-              <div class = "col align-self-center" style = "text-align: center;">
-                  <h1>"Untitled"</h1>
-              </div>
-              <div class = "col-sm-2">
-                  <div>
-                       Date
-                  </div>
-                  </div>
-              <div class = "col-sm-6" style = "">
-                   <div style="text-align: center;">
-                       <span style="font-size:4em">X</span> <br /> activities accomplished today!
+               <div class='card' style="height:30em">
+                    <div class="card-header" style="text-align:center">
+                         <h1>"Untitled"</h1>
+                    </div>
+                   <div class = "row">
+                   <!-- <div class = "col align-self-center" style = "text-align: center;">
+
+                   </div> -->
+                   <div class = "col-sm-3" style="margin:auto">
+                       <div style="margin:auto;">
+                            <div style="font-size:10em">
+                                 24
+                            </div>
+                            <div style="font-size:2em">
+                                 February 2019
+                            </div>
+
+                          <div>
+
+                            </div>
+                       </div>
                    </div>
-                  <div class="card task" style="width: 25rem; margin:auto;text-align:center ">
-                    <ul class="list-group list-group-flush" style="font-size:2em">
-                      <li class="list-group-item">Cras justo odio</li>
-                      <li class="list-group-item">Dapibus ac facilisis in</li>
-                      <li class="list-group-item">Vestibulum at eros</li>
-                    </ul>
-                  </div>
-              </div>
+
+                   <div class = "col-sm-6" style = "">
+                        <div style="text-align: center;padding-bottom:2em;">
+                            <span style="font-size:4em">X</span> <br /> activities accomplished today!
+                        </div>
+                       <div class="card task" style="width: 25rem; margin:auto;text-align:center ">
+                         <ul class="list-group list-group-flush" style="font-size:2em">
+                           <li class="list-group-item">Cras justo odio</li>
+                           <li class="list-group-item">Dapibus ac facilisis in</li>
+                           <li class="list-group-item">Vestibulum at eros</li>
+                         </ul>
+                       </div>
+                   </div>
+                   </div>
               </div>
 
 
