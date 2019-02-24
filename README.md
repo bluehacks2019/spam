69dbc2d3cf2700244bd695d8fc9d35c37ca38530
aaroncolesee <aaron.see@obf.ateneo.edu>
2019-02-24 08:35:47 +0800

    fixes

--- README.md
+++ README.md
@@ -11,7 +11,11 @@ Run the command "composer update"
 Run the command "php artisan key:generate"
 Run the command "php artisan storage:link"
 
-Create a database with the name 
-Run the command "php artisan migrate:refresh"
+Create a database with the name you put in the .env file
 
 Run the command "php artisan serve"
+
+Run the command "php artisan migrate:refresh"
+Register as a user
+Populate the database using the files inside the _sqlData folder
+Run the command "php artisan db:seed"
---
+++ _sqlData/populate.sql
@@ -0,0 +1,39 @@
+INSERT INTO activities (name, genre, description)
+VALUES ('Drink 8 glasses of water','physical','Hydrate your body, rid your it of toxins, and keep yourself energized');
+INSERT INTO activities (name, genre, description)
+VALUES ("Don't eat sweets",'physical','Lose weight, have clearer skin, and keep your brain sharp.');
+INSERT INTO activities (name, genre, description)
+VALUES ('Take a short walk','emotional','Lessen your stress by walking around your neighborhood. This gives more head space and allows one to reflect and meditate.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Read an article on a topic that intrigues you",'intellectual','Learn something new today! Stay mentally stimulated to keep your brain in top shape.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Learn a new word and use it",'intellectual','Learn something new today! Expand your vocabulary.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Practice mindfulness for 10 minutes",'emotional','Find a place to sit, sit in a comfortable position and breathe. Be mindful of when your mind wanders and go back when it happens.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Write a diary entry on your day",'emotional','Jot down what you feel, find the target stressors and plan out ways to solve them.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Have lunch with a friend",'social','Humans are naturally social, although some greater than others. Hanging out with friends and strengthen your relationships.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Call a friend",'social','Strengthen your relationships, communicate with others.');
+INSERT INTO activities (name, genre, description)
+VALUES ("30 Star jumps",'physical','From squat position, jump up and spread your arms and legs wide. Land slowly and bend your knees, back into squat position.');
+INSERT INTO activities (name, genre, description)
+VALUES ("10000 steps",'physical','10000 steps is the minimum daily amount you should walk to be considered active.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Try a healthy restaurant",'physical','Explore different cuisine, without it attacking your diet.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Focus on your breathing",'emotional','This nurtures mindfulness, and allows one to relax.');
+INSERT INTO activities (name, genre, description)
+VALUES ("No caffeine after 1pm",'physical','To avoid sleeping problems, cut off the caffiene at 1pm.');
+INSERT INTO activities (name, genre, description)
+VALUES ("Eat green veggies with dinner",'physical','Eat healthier in moderation, add greens to a meal.');
+INSERT INTO activities (name, genre, description)
+VALUES ("3 minutes of dancing",'physical','Improve the condition of your heart, lungs, muscles and more!');
+INSERT INTO activities (name, genre, description)
+VALUES ("4-7-8 Deep breathing exercise",'emotional','Exhale, inhale for four seconds, hold your breath for seven and exhale for a duration of 8 seconds.');
+
+
+
+
+
