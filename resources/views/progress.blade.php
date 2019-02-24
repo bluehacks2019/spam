b82171a12e5b3010ad98ebf53542a7128e00a447
Deion Menor <deionmenor@gmail.com>
2019-02-24 12:19:41 +0800

    More CSS fixes

--- resources/views/progress.blade.php
+++ resources/views/progress.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-<div class="bg ">
+<div class="bg-profile" style="padding-bottom:150px;">
 
-    <div class = "d-flex flex-column justify-content-center" style = "text-align:center; padding-top: 30px;">
+    <div class = "d-flex flex-column justify-content-center" style = "text-align:center; padding-top: 90px;">
         <div><img src="https://thumbs.dreamstime.com/z/stressed-student-24485690.jpg" alt="My face" class="rounded-circle"></div>
         <div class="p-2" style = "font-size: 40px;font-family:'HWT Slab'">{{Auth::user()->name}}</div>
-        <div class="p-2" style = "font-size: 20px;">A student hoping to become a more mindful person</div>
+        <div class="p-2" style = "font-size: 30px;font-family:'RiftSoft';">A student hoping to become a more mindful person</div>
     </div>
     <div class = "row">
         <div class= "col-sm-6" align = "center">
@@ -19,36 +19,39 @@
     </div>
     <div class = "row">
         <div class = "col-sm-6" align = "center">
-            <p>activities accomplished</p>
+            <p style="font-family:RiftSoft; font-size:30px;color:#FEFEFE;padding-top:50px;">activities accomplished</p>
         </div>
         <div class = "col-sm-6" align = "center">
-            <p>day streak</p>
+            <p style="font-family:RiftSoft; font-size:30px;color:#FEFEFE;padding-top:50px;">day streak</p>
         </div>
     </div>
 
+</div>
 
-   <h1>TROPHIES</h1>
+<div style="margin-left:139px;margin-top:150px;">
+  <h1 style ="font-family:'HWT Slab';font-size:80px;color:#434244;">trophies</h1>
 
-<div class="h-100 row align-items-center">
-     <div class="card" style="width: 18rem; padding:10px">
+ <div class="h-100 row align-items-center" style="margin-top:50px; margin-bottom:125px;">
+       <div class="card" style="width: 18rem; padding:10px; border:0px;">
+       <img class="card-img-top" src="http://media.gettyimages.com/vectors/vector-trophy-illustration-vector-id530286335?s=170x170" alt="Card image cap">
+       <div class="card-body">
+         <p class="card-text" style="font-family:'RiftSoft';font-size:20px;text-align:center;">50 activities done</p>
+       </div>
+      </div>
+      <div class="card" style="width: 18rem;padding:10px; border:0px;">
       <img class="card-img-top" src="http://media.gettyimages.com/vectors/vector-trophy-illustration-vector-id530286335?s=170x170" alt="Card image cap">
       <div class="card-body">
-        <p class="card-text">Write your first entry!</p>
+         <p class="card-text" style="font-family:'RiftSoft';font-size:20px;text-align:center;">30-day streak</p>
       </div>
-    </div>
-    <div class="card" style="width: 18rem;padding:10px">
-    <img class="card-img-top" src="http://media.gettyimages.com/vectors/vector-trophy-illustration-vector-id530286335?s=170x170" alt="Card image cap">
-    <div class="card-body">
-       <p class="card-text">Finish 5 fulfilling activities in a single day!</p>
-    </div>
-   </div>
-   <div class="card" style="width: 18rem;padding:10px">
-   <img class="card-img-top" src="http://media.gettyimages.com/vectors/vector-trophy-illustration-vector-id530286335?s=170x170" alt="Card image cap">
-   <div class="card-body">
-      <p class="card-text">Write 5 entries in a row!</p>
-   </div>
-   </div>
-</div>
+     </div>
+     <div class="card" style="width: 18rem;padding:10px; border:0px;">
+     <img class="card-img-top" src="http://media.gettyimages.com/vectors/vector-trophy-illustration-vector-id530286335?s=170x170" alt="Card image cap">
+     <div class="card-body">
+       <p class="card-text" style="font-family:'RiftSoft';font-size:20px;text-align:center;">25 activities done</p>
+     </div>
+     </div>
+ </div>
+
 
 </div>
 
